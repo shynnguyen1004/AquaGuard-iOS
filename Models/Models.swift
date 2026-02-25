55af2f2bdc3cab38b3f0ddd86835cf7c21b1544c
shynnguyen <truongson10042007@gmail.com>
2026-02-25 19:52:42 +0700

    fix: move RescueResource/RescueRequest structs to Models.swift
    Model structs should be defined in the Models layer, not in ViewModels.
    This fixes 'Cannot find in scope' build errors in Xcode since Models.swift
    was referencing types that were defined in RescueViewModel.swift.

--- Models/Models.swift
+++ Models/Models.swift
@@ -144,3 +144,22 @@ struct DataPackage {
     let number: String
     let syntax: String
 }
+
+struct RescueResource: Identifiable {
+    let id = UUID()
+    let icon: String
+    let title: String
+    let current: Int
+    let total: Int
+    let color: Color
+}
+
+struct RescueRequest: Identifiable {
+    let id = UUID()
+    let address: String
+    let people: Int
+    let time: String
+    let status: String
+    let team: String?
+    let severityColor: Color
+}
--- ViewModels/RescueViewModel.swift
+++ ViewModels/RescueViewModel.swift
@@ -5,32 +5,7 @@
 //  Created for P1 refactor: extract hardcoded data from RescueView.
 //
 
-import Combine
 import Foundation
-import SwiftUI
-
-// MARK: - Models
-
-struct RescueResource: Identifiable {
-    let id = UUID()
-    let icon: String
-    let title: String
-    let current: Int
-    let total: Int
-    let color: Color
-}
-
-struct RescueRequest: Identifiable {
-    let id = UUID()
-    let address: String
-    let people: Int
-    let time: String
-    let status: String
-    let team: String?
-    let severityColor: Color
-}
-
-// MARK: - ViewModel
 
 @MainActor
 class RescueViewModel: ObservableObject {
